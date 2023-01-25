adc9fa6e7eea7d2769302659cf113cc7d440274b
abdullahberkeozder <abdullahberkeozder@gmail.com>
2023-01-25 13:43:19 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int math, phys, eng, music, chem;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter math Exam: ");
+        math = inp.nextInt();
+        System.out.print("Enter phys Exam: ");
+        phys = inp.nextInt();
+        System.out.print("Enter eng Exam: ");
+        eng = inp.nextInt();
+        System.out.print("Enter music Exam: ");
+        music = inp.nextInt();
+        System.out.print("Enter chem Exam: ");
+        chem = inp.nextInt();
+
+        double total = (math + phys + eng + music + chem);
+        double avarage = total / 5;
+
+        
+        System.out.println("Your Avarage Exam Notes: " + avarage);
+    }
+}
